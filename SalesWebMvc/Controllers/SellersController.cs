6f76acac8edc5e42c9a56a48420a4516d1ce6072
Rubens Dias <rubensdiasneto@gmail.com>
2023-02-03 08:52:35 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -7,22 +12,28 @@ namespace SalesWebMvc.Controllers
     {
         private readonly SellerService _sellerService;
 
-        internal interface ISellerService
-        {
-        }
-
-
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
-        } 
-
-        
+        }
 
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
 
     }
